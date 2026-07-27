1d0f81428434ff2d895b82249a53edb2ef395a26
tipmate-radar <ganeevm.top@yandex.ru>
2026-07-27 15:09:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from utils.rag_store import add_summary_to_store
 
 def classify_rubric(article) -> str:
     text = (article.get("title", "") + " " + article.get("link", "")).lower()
-   if any(k in text for k in [
+    if any(k in text for k in [
         "tipjar", "tipsi", "globaltips", "global.tips", "edrixx", "sunday",
         "sipay", "sipos", "tipplus", "tipead", "tiepad",
         "tipsyou", "tap tiiip", "taptiiip", "tippie", "tackpay", "justtip"
